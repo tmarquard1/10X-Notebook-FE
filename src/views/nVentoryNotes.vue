b2fc7422f17f93f689cdd302e1600789d8bf7579
TiffanyClark225 <tncl225@uky.edu>
2023-04-20 13:42:44 +0000

    Added a title text box & you can load notes on the page created 4.16.2023

--- src/views/nVentoryNotes.vue
+++ src/views/nVentoryNotes.vue
@@ -2,39 +2,73 @@
     <div class="container">
         <div class="row mt-4">
             <h1>NVENTORY NOTES</h1>
+            <input v-model="title" placeholder="Title">
         </div>
     </div>
     <div class="row mt-4">
         <div class="col-md-6">
             <h4>Markdown</h4>
-            <textarea v-model="markdown" class="info"></textarea>
+            <textarea v-model="content" class="info"></textarea>
         </div>
         <div class="col-md-6">
             <h4>Formatted Text</h4>
             <div v-html="markdownToHtml" class="info"></div>
         </div>
     </div>
-    <button @click="addNote">Save</button>
+    <button @click="create">Save Note</button>
+    <p>{{notesData}}</p>
+    <button @click="getNotes">Load Notes</button>
+    <div v-for="note in notes" :key="note.id" class="info">
+      <h2>Note {{ note.id }}: {{ note.title }}</h2>
+        <p></p>
+        <h5>{{ note.content }}</h5>
+    </div>
 </template>
 
 
 <script>
 import {marked} from 'marked';
-
+import axios from 'axios'
 export default {
-name: 'App',
-components: {
-},
-data() {
-    return {
-    markdown: 'Enter Markdown Here...',
-    };
-},
-computed: {
-    markdownToHtml() {
-    return marked(this.markdown);
+    name: 'nventory',
+    data() {
+        return {
+            content: 'Enter Markdown Here...',
+            notes: [],
+            title: '',
+            notesData:{}
+        }
+    },
+    methods: {
+        getNotes() {
+            axios.get('http://localhost:9000/notes/')
+                .then((response) => {
+                    console.log(response.data)
+                    this.notes = response.data
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        },
+        create() {
+            const data = {
+                title: this.title,
+                content: this.content
+            };
+            axios.post('http://localhost:9000/note/add', data)
+                .then(response => {
+                    console.log(response);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    },
+    computed: {
+        markdownToHtml() {
+        return marked(this.content);
+        }
     }
-}
 
 }
 
@@ -63,9 +97,21 @@ h1 {
     margin: 0
 }
 
+h3 {
+    color: rgb(16, 165, 100);
+    text-align: center;
+    margin: 0
+}
+
 h4 {
     text-align: center;
     
+}
+
+button {
+  display: block;
+  width: 100px;
+  margin: 15 px;
 }
 
 </style>
--- src/views/xPortNotes.vue
+++ src/views/xPortNotes.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="container">
-    <div class="row mt-4">
-      <h1>ALL NOTES</h1>
-    </div>
-  </div>
-  <div class="row mt-4">
-    <div class="col-md-6">
-      <button @click="getNotes">Load Notes</button>
-      <div v-for="note in notes" :key="note.id">
-        <h5>{{ note.id }}. {{ note.title }}. {{ note.tags }}. {{ note.content }}</h5>
-      </div>
+    <h1>ALL NOTES</h1>
+    <button @click="getNotes">Load Notes</button>
+    <div v-for="note in notes" :key="note.id" class="info">
+      <h2>Note {{ note.id }}: {{ note.title }}</h2>
+        <p></p>
+        <h5>{{ note.content }}</h5>
     </div>
   </div>
 </template>
+
 
 <script>
 import axios from 'axios'
@@ -37,3 +34,35 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.container {
+    margin-top: 40px;
+}
+
+.info {
+    height: 200px;
+    width: 100%;
+    background-color: white;
+    border: 2px black
+    
+}
+h1 {
+    color:rgb(16, 163, 200);
+    text-align: center;
+    margin: 0
+}
+
+h3 {
+    color: rgb(16, 165, 100);
+    text-align: center;
+    margin: 0
+}
+
+button {
+  display: block;
+  width: 100px;
+  margin: 15 px;
+}
+
+</style>
